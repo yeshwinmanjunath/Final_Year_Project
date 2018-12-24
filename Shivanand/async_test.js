a449d8ff76fe6aedfe8680b42a7a3094ab704e11
Gattupalli Tejaswi <gsteja1234@gmail.com>
2018-12-24 19:30:11 +0530

    Create async_test.js

---
+++ Shivanand/async_test.js
@@ -0,0 +1,15 @@
+const {fork}=require('child_process')
+
+const forked=fork('mine.js')
+
+var timer1 = setTimeout(() =>{  /*This is demonstration purpose only, this will be an
+                                  event listener for incoming mined blocks*/
+        forked.kill('SIGINT')
+        console.log('Mining stopped due to finished block from some other node.')    
+},10)
+
+forked.on('message',(msg)=>{
+    clearTimeout(timer1)
+    console.log('Mining completed in this node itself')
+    console.log(`Mining result: ${msg}`)
+})
